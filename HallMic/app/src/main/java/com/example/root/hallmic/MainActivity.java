42a6ac1ca20b9fff13e1f7f16693ff3a4e7b9744
Utkarsh Kumar <kumar.utkarsh007@gmail.com>
2015-02-16 02:02:53 +0530

    A basic sound recorder! Yetto figure out a way to send the recorded sound instantaneously...

--- HallMic/app/src/main/java/com/example/root/hallmic/MainActivity.java
+++ HallMic/app/src/main/java/com/example/root/hallmic/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.root.hallmic;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +35,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startRecording(View view){
+        Intent intent = new Intent(this, RecordActivity.class);
+        startActivity(intent);
     }
 }
